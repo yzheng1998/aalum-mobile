b94eabc5ed37e79c603f539fca78c69bc4b4251d
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-11-27 19:06:52 -0500

    create temporary signin screen

---
+++ src/screens/SignIn/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react-native'
+import { Screen } from './styles'
+
+export default class Temp extends Component {
+  render() {
+    return <Screen />
+  }
+}
---
+++ src/screens/SignIn/styles.js
@@ -0,0 +1,3 @@
+import styled from 'styled-components'
+
+export const Screen = styled.View``
