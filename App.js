7e783961467734d8868cfc616eb407254c2bf7bb
Kyle Sutton <ksutton@Kyle-Suttons-MacBook-Pro.local>
2018-11-28 17:25:27 -0500

    created themeprovider

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import { ApolloProvider } from 'react-apollo'
+import { ThemeProvider } from 'styled-components/native'
 import { client } from './src/apollo'
 import RootNavigator from './src/navigation/RootNavigator'
+import theme from './theme'
 
 export default class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <RootNavigator />
+        <ThemeProvider theme={theme}>
+          <RootNavigator />
+        </ThemeProvider>
       </ApolloProvider>
     )
   }
---
+++ theme.js
@@ -0,0 +1,17 @@
+const theme = {
+  colors: {
+    white: '#FFFFFF',
+    gradientLeft: '#DA2752',
+    gradientRight: '#FF6534',
+    grey: '#A8A8A8',
+    charcoal: '#333333',
+    offWhite: '#F9F9F9'
+  },
+  fonts: {
+    heading: 'Montserrat-Bold',
+    title: 'Avenir-Heavy',
+    bodyText: 'Avenir-Medium'
+  }
+}
+
+export default theme
