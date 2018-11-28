304111054fae5cf42545c8a4cc913757f420fd9c
ChrisMunene <munenechristoph@gmail.com>
2018-11-28 18:38:19 -0500

    Fix margins in title

--- src/components/Title/styles.js
+++ src/components/Title/styles.js
@@ -2,7 +2,10 @@ import styled from 'styled-components'
 import theme from '../../../theme'
 
 export const Text = styled.Text`
+  text-align: center;
   align-self: center;
+  margin-left: 32px;
+  margin-right: 32px;
   font-size: 28px;
   margin-top: 88px;
   font-family: ${theme.fonts.title};
