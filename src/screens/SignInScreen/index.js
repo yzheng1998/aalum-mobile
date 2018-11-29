3265f528b8504ec41fd4227d8ead59d563d36104
Kyle Sutton <ksutton@Kyle-Suttons-MacBook-Pro.local>
2018-11-29 15:32:46 -0500

    integrated PrimaryInput into sign in screen

--- src/screens/SignInScreen/index.js
+++ src/screens/SignInScreen/index.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react'
-import { Screen } from './styles'
 import PrimaryButton from '../../components/PrimaryButton'
+import { Screen, TextInputContainer } from './styles'
+import PrimaryInput from '../../components/PrimaryInput'
+import Icon from 'react-native-vector-icons/Ionicons'
+import Lock from 'react-native-vector-icons/Foundation'
 
-export default class Temp extends Component {
+export default class SignUpScreen extends Component {
+  state = {
+    email: '',
+    password: '',
+    emailError: '',
+    passwordError: ''
+  }
   render() {
+    const { passwordError, emailError, email, password } = this.state
     return (
       <Screen>
+        <TextInputContainer>
+          <PrimaryInput
+            error={emailError}
+            placeholder={email || 'Email'}
+            icon={
+              <Icon name="md-person" size={20} color="rgb(181, 171, 202)" />
+            }
+            autoCapitalize="none"
+          />
+        </TextInputContainer>
+        <TextInputContainer>
+          <PrimaryInput
+            secureTextEntry
+            error={passwordError}
+            placeholder={password || 'Password'}
+            icon={<Lock name="lock" size={20} color="rgb(181, 171, 202)" />}
+            autoCapitalize="none"
+          />
+        </TextInputContainer>
         <PrimaryButton
           title="Sign In"
           onPress={() => {
--- src/screens/SignInScreen/styles.js
+++ src/screens/SignInScreen/styles.js
@@ -4,3 +4,7 @@ export const Screen = styled.View`
   flex: 1;
   justify-content: center;
 `
+
+export const TextInputContainer = styled.View`
+  margin: 10px 0px 10px 0px;
+`
