966377e36d8649ac92045de4401d08fde108f71d
ChrisMunene <munenechristoph@gmail.com>
2018-11-30 18:45:55 -0500

    Merge

--- src/screens/VerificationScreen/index.js
+++ src/screens/VerificationScreen/index.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import { Screen } from './styles'
 import { Title } from '../../components/Title'
 import { Subtitle } from '../../components/Subtitle'
+import BackButton from '../../components/BackButton'
 
 export default class VerificationScreen extends Component {
   render() {
     return (
       <Screen>
+        <BackButton />
         <Title> Verify your email </Title>
         <Subtitle>
           Enter the 4 digit code we sent you to verify your account
