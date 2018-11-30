082989fae62262eadb913ad2c24bbe8214387e88
ChrisMunene <munenechristoph@gmail.com>
2018-11-30 18:19:51 -0500

    Use the new title and subtitle in verification screen

--- src/screens/VerificationScreen/index.js
+++ src/screens/VerificationScreen/index.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import { Screen } from './styles'
-import Title from '../../components/Title'
-import Subtitle from '../../components/Subtitle'
+import { Title } from '../../components/Title'
+import { Subtitle } from '../../components/Subtitle'
 
 export default class VerificationScreen extends Component {
   render() {
     return (
       <Screen>
-        <Title text="Verify your email" />
-        <Subtitle text="Enter the 4 digit code we sent you to verify your account" />
+        <Title> Verify your email </Title>
+        <Subtitle>
+          Enter the 4 digit code we sent you to verify your account
+        </Subtitle>
       </Screen>
     )
   }
