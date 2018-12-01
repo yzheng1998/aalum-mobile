e006ff270846d65649b38174926275917e471d4f
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-12-01 10:37:27 -0500

    fix title margin

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -4,8 +4,8 @@ import theme from '../../../theme'
 export const Title = styled.Text`
   text-align: center;
   align-self: center;
-  margin-left: 12px;
-  margin-right: 12px;
+  margin-left: 20px;
+  margin-right: 20px;
   font-size: 28px;
   margin-top: 48px;
   font-family: ${theme.fonts.title};
