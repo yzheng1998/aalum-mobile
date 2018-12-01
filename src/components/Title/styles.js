de657c0d36075871cf44b2316e17746f8e4072bb
ChrisMunene <munenechristoph@gmail.com>
2018-11-30 19:38:23 -0500

    Change top margin to 48px

---
+++ src/components/Title/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+import theme from '../../../theme'
+
+export const Text = styled.Text`
+  text-align: center;
+  align-self: center;
+  margin-left: 32px;
+  margin-right: 32px;
+  font-size: 28px;
+  margin-top: 48px;
+  font-family: ${theme.fonts.title};
+  color: ${theme.colors.charcoal};
+`
