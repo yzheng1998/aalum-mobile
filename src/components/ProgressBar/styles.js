103a5bd4360fb7f7fca7a6fc4476c9c3452bdbcc
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-12-04 14:20:44 -0500

    create progress bar

---
+++ src/components/ProgressBar/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+import { themeGet } from 'styled-system'
+
+export const EmptyBar = styled.View`
+  background-color: ${themeGet('colors.grey')};
+  height: 4px;
+`
