099cd80613afcf6a3de623cc098c9d81d1748a18
ChrisMunene <munenechristoph@gmail.com>
2018-12-04 21:20:53 -0500

    Add Genders to Gender screen

--- src/screens/RegistrationScreens/GenderScreen/index.js
+++ src/screens/RegistrationScreens/GenderScreen/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import RegistrationScreen from '../../../components/RegistrationScreen'
 import PrimaryButton from '../../../components/PrimaryButton'
+import GenderButtonList from '../../../components/GenderButtonList'
 
 export default class GenderScreen extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class GenderScreen extends Component {
         title="Which gender(s) best describes you?"
         progress="66%"
       >
+        <GenderButtonList />
         <PrimaryButton
           title="Continue"
           onPress={() => this.props.navigation.navigate('Seeking')}
