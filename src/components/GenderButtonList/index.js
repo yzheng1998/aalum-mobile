ac6dfec2e3fb98351bf9d8103da896cee27d7b6c
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-12-05 16:00:30 -0500

    ,add gender button list to gender screen

--- src/components/GenderButtonList/index.js
+++ src/components/GenderButtonList/index.js
@@ -1,33 +1,17 @@
 import React, { Component } from 'react'
-import { Genders } from '../../../constants'
 import { ButtonContainer } from './styles'
 import GenderButton from '../GenderButton'
 
 export default class GenderButtonList extends Component {
-  state = {
-    selectedGenders: [],
-    genders: Genders
-  }
-
-  selectGender = (gender, index) => {
-    let { selectedGenders, genders } = this.state
-    genders[index].selected = !genders[index].selected
-    selectedGenders = selectedGenders.filter(g => g.title !== gender.title)
-    this.setState({
-      genders,
-      selectedGenders
-    })
-  }
-
   render() {
-    const { genders } = this.state
+    const { genderSelection, selectGender } = this.props
     return (
       <ButtonContainer>
-        {genders.map((gender, index) => (
+        {genderSelection.map((gender, index) => (
           <GenderButton
             key={gender.title}
             title={gender.title}
-            selectGender={() => this.selectGender(gender, index)}
+            selectGender={() => selectGender(index)}
             selected={gender.selected}
           />
         ))}
--- src/screens/RegistrationScreens/GenderScreen/index.js
+++ src/screens/RegistrationScreens/GenderScreen/index.js
@@ -2,9 +2,31 @@ import React, { Component } from 'react'
 import RegistrationScreen from '../../../components/RegistrationScreen'
 import PrimaryButton from '../../../components/PrimaryButton'
 import GenderButtonList from '../../../components/GenderButtonList'
+import { GenderList } from '../../../../constants'
+
+const initialGenderSelection = GenderList.map(title => ({
+  title,
+  selected: false
+}))
 
 export default class GenderScreen extends Component {
+  state = {
+    genderSelection: initialGenderSelection
+  }
+
+  selectGender = index => {
+    const { genderSelection } = this.state
+    genderSelection[index].selected = !genderSelection[index].selected
+    this.setState({
+      genderSelection
+    })
+  }
+
   render() {
+    const finalGenders = this.state.genderSelection
+      .filter(gender => gender.selected)
+      .map(gender => gender.title)
+
     return (
       <RegistrationScreen
         showBack
@@ -12,10 +34,15 @@ export default class GenderScreen extends Component {
         title="Which gender(s) best describes you?"
         progress="66%"
       >
-        <GenderButtonList />
+        <GenderButtonList
+          genderSelection={this.state.genderSelection}
+          selectGender={this.selectGender}
+        />
         <PrimaryButton
           title="Continue"
-          onPress={() => this.props.navigation.navigate('Seeking')}
+          onPress={() =>
+            this.props.navigation.navigate('Seeking', { genders: finalGenders })
+          }
         />
       </RegistrationScreen>
     )
