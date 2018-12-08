3c2031f323187c55720fe67c0a35c4d1a1c3657e
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-12-08 17:26:37 -0500

    seeking gender toggle

--- src/screens/RegistrationScreens/SeekingGenderScreen/index.js
+++ src/screens/RegistrationScreens/SeekingGenderScreen/index.js
@@ -3,6 +3,8 @@ import RegistrationScreen from '../../../components/RegistrationScreen'
 import PrimaryButton from '../../../components/PrimaryButton'
 import GenderButtonList from '../../../components/GenderButtonList'
 import { GenderList } from '../../../../constants'
+import { ToggleContainer, Toggle, ToggleText } from './styles'
+import theme from '../../../../theme'
 
 const initialGenderSelection = GenderList.map(title => ({
   title,
@@ -23,11 +25,16 @@ export default class SeekingGenderScreen extends Component {
   }
 
   render() {
-    const finalGenders = this.state.genderSelection
+    const { genderSelection } = this.state
+    const finalGenders = genderSelection
       .filter(gender => gender.selected)
       .map(gender => gender.title)
-    const enabled =
-      this.state.genderSelection.filter(gender => gender.selected).length > 0
+    const numberSelected = this.state.genderSelection.filter(
+      gender => gender.selected
+    ).length
+    const enabled = numberSelected > 0
+
+    const allSelected = numberSelected === genderSelection.length
     return (
       <RegistrationScreen
         showBack
@@ -36,10 +43,31 @@ export default class SeekingGenderScreen extends Component {
         progress="80%"
       >
         <GenderButtonList
-          genderSelection={this.state.genderSelection}
+          genderSelection={genderSelection}
           selectGender={this.selectGender}
+          allSelected={allSelected}
         />
+        <ToggleContainer>
+          <Toggle
+            trackColor={{
+              false: theme.colors.grey,
+              true: theme.colors.gradientRight
+            }}
+            ios_backgroundColor={theme.colors.grey}
+            value={allSelected}
+            onValueChange={isOn =>
+              this.setState(prevState => ({
+                genderSelection: prevState.genderSelection.map(gender => ({
+                  title: gender.title,
+                  selected: isOn
+                }))
+              }))
+            }
+          />
+          <ToggleText>I&apos;m open to everyone</ToggleText>
+        </ToggleContainer>
         <PrimaryButton
+          style={{ marginTop: 22 }}
           title="Continue"
           onPress={() =>
             this.props.navigation.navigate('Photo', { seeking: finalGenders })
