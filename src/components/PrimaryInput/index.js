8b8386e1e964cd6b3b91fdbf207261713bac3928
Kyle Sutton <ksutton@Kyle-Suttons-MacBook-Pro.local>
2018-12-11 17:15:15 -0500

    removed commented code, linted

--- src/components/PrimaryInput/index.js
+++ src/components/PrimaryInput/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Platform } from 'react-native'
 import {
   InputField,
   RoundContainer,
--- src/screens/RegistrationScreens/SignInScreen/index.js
+++ src/screens/RegistrationScreens/SignInScreen/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Dimensions, View } from 'react-native'
+import { Dimensions } from 'react-native'
 import {
   GreyBar,
   TabContainer,
@@ -11,27 +11,12 @@ import {
   imageStyle
 } from './styles'
 import Logo from '../../../../assets/images/registrationFlow/Logo.png'
-import { TabView, TabBar, TabViewPage, SceneMap } from 'react-native-tab-view'
+import { TabView, TabBar, SceneMap } from 'react-native-tab-view'
 import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
 import RegistrationScreen from '../../../components/RegistrationScreen'
 
-// const FirstRoute = props => <SignIn />
-
-// const SecondRoute = props => <SignUp />
-
 export default class SignInScreen extends Component {
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //     index: 0,
-  //     routes: [
-  //       { key: 'first', title: 'Sign In' },
-  //       { key: 'second', title: 'Sign Up' }
-  //     ]
-  //   }
-  // }
-
   state = {
     index: 0,
     routes: [
@@ -55,30 +40,13 @@ export default class SignInScreen extends Component {
     />
   )
 
-  // renderScene = ({ route }) => {
-  //   switch (route.key) {
-  //     case 'first':
-  //       return <SignIn navigation={props.navigation} />
-  //     case 'second':
-  //       return <SignUp navigation={props.navigation} />
-  //     default:
-  //       return <View />
-  //   }
-  // }
-
-  // renderPage = props => {
-  //   return <TabViewPage {...props} renderScene={this.renderScene} />
-  // }
-
   render() {
-    // const { FirstRoute, SecondRoute } = this.state
     return (
       <RegistrationScreen source={Logo} imageStyle={imageStyle}>
         <TabContainer>
           <GreyBar />
           <TabView
             navigationState={this.state}
-            // renderScene={this.renderPage}
             renderScene={SceneMap({
               first: this.FirstRoute,
               second: this.SecondRoute
@@ -96,56 +64,3 @@ export default class SignInScreen extends Component {
     )
   }
 }
-
-// import React, { Component } from 'react'
-// import { View, StyleSheet, Dimensions } from 'react-native'
-// import { TabView, TabBar, SceneMap } from 'react-native-tab-view'
-
-// const FirstRoute = props => <SignIn navigation={props.navigation} />
-// const SecondRoute = props => <SignUp navigation={props.navigation} />
-
-// export default class TabViewExample extends Component {
-//   state = {
-//     index: 0,
-//     routes: [
-//       { key: 'first', title: 'Sign In' },
-//       { key: 'second', title: 'Sign Up' }
-//     ]
-//   }
-
-//   renderTabBar = props => (
-//     <TabBar
-//       {...props}
-//       scrollEnabled
-//       tabStyle={tabStyle}
-//       labelStyle={labelStyle}
-//       style={tabBarStyle}
-//       indicatorStyle={indicatorStyle}
-//     />
-//   )
-
-//   render() {
-//     return (
-//       <RegistrationScreen source={Logo} imageStyle={imageStyle}>
-//         <TabContainer>
-//           <TabView
-//             navigationState={this.state}
-//             renderScene={SceneMap({
-//               first: () => FirstRoute(this.props),
-//               second: () => SecondRoute(this.props)
-//             })}
-//             onIndexChange={index => this.setState({ index })}
-//             initialLayout={{ width: Dimensions.get('window').width }}
-//             renderTabBar={this.renderTabBar}
-//           />
-//         </TabContainer>
-//       </RegistrationScreen>
-//     )
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   scene: {
-//     flex: 1
-//   }
-// })
