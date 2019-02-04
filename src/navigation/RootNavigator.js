3c51e7f3464772529ebb4ba329d6e7a9d9f6d141
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2019-02-03 22:00:36 -0500

    Func(rootNavigator)

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react'
-import { createAppContainer } from 'react-navigation'
+import { createSwitchNavigator, createAppContainer } from 'react-navigation'
 import RegistrationStack from './RegistrationStack'
+import DiscoverStack from './DiscoverStack'
+import { AsyncStorage, Alert } from 'react-native'
+import LoadingWrapper from '../components/LoadingWrapper'
+
+const isSignedIn = async () => {
+  const token = await AsyncStorage.getItem('token')
+  return token !== null
+}
+
+const createRootNavigator = (loggedIn = false) =>
+  createSwitchNavigator(
+    {
+      Registration: {
+        screen: RegistrationStack
+      },
+      Discover: {
+        screen: DiscoverStack
+      }
+    },
+    {
+      initialRouteName: loggedIn ? 'Discover' : 'Registration',
+      headerMode: 'screen'
+    }
+  )
 
 export default class RootNavigator extends Component {
+  state = {
+    signedIn: false,
+    loading: true
+  }
+
+  componentDidMount() {
+    isSignedIn()
+      .then(res => this.setState({ signedIn: res, loading: false }))
+      .catch(() => Alert.alert('An error occurred'))
+  }
   render() {
-    const RootNavigatorComponent = createAppContainer(RegistrationStack)
+    const { loading, signedIn } = this.state
+    if (loading) {
+      return <LoadingWrapper loading={loading} />
+    }
+    const RootNavigatorComponent = createAppContainer(
+      createRootNavigator(signedIn)
+    )
     return <RootNavigatorComponent />
   }
 }
