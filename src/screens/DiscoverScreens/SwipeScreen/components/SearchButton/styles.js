86025888013af89e4d12a1c0ffe9cfc4d8808d79
Mohamed Ally Iddi <mohamed_iddi@college.harvard.edu>
2019-02-07 23:43:21 -0500

    Styling(SearchButton) -- Built search button

---
+++ src/screens/DiscoverScreens/SwipeScreen/components/SearchButton/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+import { themeGet } from 'styled-system'
+
+export const Container = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-left: 25px;
+  margin-right: 25px;
+  margin-bottom: 37px;
+  padding-left: 15px;
+  height: 50px;
+  background-color: white;
+  box-shadow: 0px 6px 25px ${themeGet('colors.shadow')};
+`
+
+export const Title = styled.Text`
+  margin-left: 15px;
+  font-size: 16px;
+  font-family: ${themeGet('fonts.bodyText')};
+  color: ${themeGet('colors.inactiveText')};
+`
