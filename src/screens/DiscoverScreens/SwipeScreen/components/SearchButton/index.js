41f3b37104598b8f6f13e43b243753db6dd8db84
Mohamed Ally Iddi <mohamed_iddi@college.harvard.edu>
2019-02-08 15:55:04 -0500

    Fix(SearchButton) -- changed SearchButton to SFC

--- src/screens/DiscoverScreens/SwipeScreen/components/SearchButton/index.js
+++ src/screens/DiscoverScreens/SwipeScreen/components/SearchButton/index.js
@@ -1,25 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { TouchableOpacity } from 'react-native'
 import { Container, Title } from './styles'
 import Icon from '../../../../../components/Icon'
 import theme from '../../../../../../theme'
 
-export default class SearchButton extends Component {
-  render() {
-    return (
-      <TouchableOpacity
-        onPress={() => this.props.navigation.navigate('Search')}
-      >
-        <Container>
-          <Icon
-            name="search"
-            size={20}
-            borderRadius={10}
-            color={theme.colors.mediumGrey}
-          />
-          <Title>Search for a user</Title>
-        </Container>
-      </TouchableOpacity>
-    )
-  }
-}
+const SearchButton = ({ navigation }) => (
+  <TouchableOpacity onPress={() => navigation.navigate('Search')}>
+    <Container>
+      <Icon name="search" size={20} color={theme.colors.mediumGrey} />
+      <Title>Search for a user</Title>
+    </Container>
+  </TouchableOpacity>
+)
+
+export default SearchButton
