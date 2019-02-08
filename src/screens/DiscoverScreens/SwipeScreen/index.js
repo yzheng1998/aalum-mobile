7cebf38ad023a4355a0b55faf529cb16ef7ddf3b
Mohamed Ally Iddi <mohamed_iddi@college.harvard.edu>
2019-02-07 23:43:34 -0500

    Styling(UserCard) -- Added search button to UserCard

--- src/screens/DiscoverScreens/SwipeScreen/index.js
+++ src/screens/DiscoverScreens/SwipeScreen/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Screen, Button } from './styles'
 import { AsyncStorage } from 'react-native'
 import UserCard from './components/UserCard'
+import SearchButton from './components/SearchButton'
 
 const SAMPLE_URL =
   'https://cdn-images-1.medium.com/max/2000/0*bdhf1cch4Mjib3UL.jpg'
@@ -16,6 +17,7 @@ export default class SwipeScreen extends Component {
     return (
       <Screen>
         <Button title="Logout" onPress={this.logout} />
+        <SearchButton navigation={this.props.navigation} />
         <UserCard picture={SAMPLE_URL} />
       </Screen>
     )
