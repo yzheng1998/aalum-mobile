6aee43ea8f9f0d098e3c69b852503f7551ba41d6
Mohamed Ally Iddi <mohamed_iddi@college.harvard.edu>
2019-02-08 16:37:46 -0500

    Styling(SearchBar) -- Built search bar

---
+++ src/screens/DiscoverScreens/SearchScreen/components/SearchBar/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { Container, Input } from './styles'
+import Icon from '../../../../../components/Icon'
+import theme from '../../../../../../theme'
+
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: ''
+    }
+  }
+  render() {
+    return (
+      <Container>
+        <Icon
+          name="search"
+          size={20}
+          borderRadius={10}
+          color={theme.colors.mediumGrey}
+        />
+        <Input
+          onKeyPress={text => this.setState({ text })}
+          placeholder="Search"
+        />
+      </Container>
+    )
+  }
+}
