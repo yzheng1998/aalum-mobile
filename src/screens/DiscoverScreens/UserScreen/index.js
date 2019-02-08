3d49e833d844078060cc38e3f6d2b4cd6788df03
Mohamed Ally Iddi <mohamed_iddi@college.harvard.edu>
2019-02-08 17:16:16 -0500

    Styling(UserScreen) -- Added UserBio to UserScreen

--- src/screens/DiscoverScreens/UserScreen/index.js
+++ src/screens/DiscoverScreens/UserScreen/index.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
 import { Screen } from './styles'
+import UserBio from './components/UserBio'
 
+const SAMPLE_TEXT =
+  'People say I’m...out of this world--but I’m just a small-town Kansas boy looking for love.'
 export default class UserScreen extends Component {
   render() {
-    return <Screen />
+    return (
+      <Screen>
+        <UserBio info={SAMPLE_TEXT} />
+      </Screen>
+    )
   }
 }
