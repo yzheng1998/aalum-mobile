b6f41e64f2491c18b8ff0cc3e9e14e053bda47e5
Hossam Mabed <hossammabed@dhcp-10-250-110-213.harvard.edu>
2019-02-10 16:10:27 -0500

    Styling(UserSearchCardPicture) -- finalized UserSearchCardPicture component

--- src/screens/DiscoverScreens/SearchScreen/components/UserSearchCard/components/UserSearchCardPicture/index.js
+++ src/screens/DiscoverScreens/SearchScreen/components/UserSearchCard/components/UserSearchCardPicture/index.js
@@ -1,17 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Pic } from './styles'
 
-class UserSearchCardPicture extends Component {
-  render() {
-    return (
-      <Pic
-        source={{
-          uri:
-            'https://media.licdn.com/dms/image/C4D03AQEimgIJC3va5g/profile-displayphoto-shrink_200_200/0?e=1549497600&v=beta&t=RpwoPYFCUcjYQbkCJeIGaEnqWfTug0k_i2brmKMIL64'
-        }}
-      />
-    )
-  }
-}
+const UserSearchCardPicture = ({ profilePicture }) => (
+  <Pic
+    source={{
+      uri: profilePicture
+    }}
+  />
+)
 
 export default UserSearchCardPicture
