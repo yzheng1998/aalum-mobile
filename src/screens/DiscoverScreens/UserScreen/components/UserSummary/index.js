5f550df50e66e5c8942636ec11314f20869895be
Hossam Mabed <hossam.nasr99@gmail.com>
2019-02-10 18:07:55 -0500

    Styling(UserSummary) -- set up component UserSummary

---
+++ src/screens/DiscoverScreens/UserScreen/components/UserSummary/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from './styles'
+
+const UserSummary = ({
+  name,
+  age,
+  distance,
+  school,
+  degree,
+  year,
+  profession
+}) => <Container />
+
+export default UserSummary
---
+++ src/screens/DiscoverScreens/UserScreen/components/UserSummary/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Container = styled.View`
+  background-color: blue;
+  height: 100px;
+  width: 100px;
+`
