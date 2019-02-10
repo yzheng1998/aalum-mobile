4c333f515ea6684357785d8cdb083220d2b3dece
Hossam Mabed <hossam.nasr99@gmail.com>
2019-02-10 17:36:16 -0500

    Styling(Match Button in User Screen) -- added MatchButton to UserScreen

--- src/screens/DiscoverScreens/UserScreen/styles.js
+++ src/screens/DiscoverScreens/UserScreen/styles.js
@@ -4,3 +4,9 @@ export const Screen = styled.View`
   flex: 1;
   padding-top: 50px;
 `
+
+export const ButtonContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-evenly;
+  margin-top: 41px;
+`
