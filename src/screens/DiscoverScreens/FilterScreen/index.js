3424d41aaeecb9b14a840d11d4e6a4b8354d469b
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2019-02-12 23:59:39 -0500

    Styling(FilterButtonGroup) -- put FilterButtonGroup on FilterScreen

--- src/screens/DiscoverScreens/FilterScreen/index.js
+++ src/screens/DiscoverScreens/FilterScreen/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Screen } from './styles'
 import ScreenHeader from '../../../components/ScreenHeader'
+import FilterButtonGroups from './components/FilterButtonGroups'
 
 export default class FilterScreen extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class FilterScreen extends Component {
           title="Filter"
           showBack
         />
+        <FilterButtonGroups />
       </Screen>
     )
   }
