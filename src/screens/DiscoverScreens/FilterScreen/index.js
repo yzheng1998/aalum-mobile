d6d73b70ef773d06f58c9ec794c256da41b4c0ef
Hossam Mabed <hossam.nasr99@gmail.com>
2019-02-13 22:10:25 -0500

    Styling(MultiSlider) -- added multi slider to filter screen

--- src/screens/DiscoverScreens/FilterScreen/index.js
+++ src/screens/DiscoverScreens/FilterScreen/index.js
@@ -2,10 +2,45 @@ import React, { Component } from 'react'
 import { Screen } from './styles'
 import ScreenHeader from '../../../components/ScreenHeader'
 import PrimaryButton from '../../../components/PrimaryButton'
+import FilterSliders from './components/FilterSliders'
 import FilterButtonGroups from './components/FilterButtonGroups'
 
 export default class FilterScreen extends Component {
+  constructor(props) {
+    super(props)
+    this.updateState = this.setState.bind(this)
+    this.state = {
+      distance: [40],
+      age: [20, 26],
+      height: [4.9, 5.8]
+    }
+  }
+
   render() {
+    const { distance, age, height } = this.state
+    const sliders = [
+      {
+        title: 'DISTANCE',
+        min: 1,
+        max: 100,
+        step: 1,
+        current: distance
+      },
+      {
+        title: 'AGE',
+        min: 18,
+        max: 60,
+        step: 1,
+        current: age
+      },
+      {
+        title: 'HEIGHT',
+        min: 0,
+        max: 9,
+        step: 0.1,
+        current: height
+      }
+    ]
     return (
       <Screen>
         <ScreenHeader
@@ -13,6 +48,7 @@ export default class FilterScreen extends Component {
           title="Filter"
           showBack
         />
+        <FilterSliders sliders={sliders} updateState={this.updateState} />
         <FilterButtonGroups />
         <PrimaryButton title="Apply" />
       </Screen>
