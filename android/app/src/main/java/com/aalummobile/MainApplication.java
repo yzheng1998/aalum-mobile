7cee2b4f037a7ade93e2f37521b4cb049e30905d
Kyle Sutton <ksutton@dhcp-10-250-51-211.harvard.edu>
2019-02-14 21:59:33 -0500

    Build (PushNotifications) -- android and ios changes

--- android/app/src/main/java/com/aalummobile/MainApplication.java
+++ android/app/src/main/java/com/aalummobile/MainApplication.java
@@ -3,6 +3,7 @@ package com.aalummobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.horcrux.svg.SvgPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new SvgPackage(),
             new RNFetchBlobPackage(),
             new PickerPackage(),
