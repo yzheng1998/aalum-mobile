234821a8e5d7aa658986fc175c03447b216ee328
Hossam Mabed <hossam.nasr99@gmail.com>
2019-02-14 22:54:46 -0500

    Func(MultiSlider) -- added height conversion functions to constants.js

--- constants.js
+++ constants.js
@@ -1,1 +1,9 @@
 export const buttonRadius = { top: 5, left: 5, bottom: 5, right: 5 }
+
+export const inchesToString = inches =>
+  `${Math.floor(inches / 12)}'${inches % 12}"`
+
+export const stringToInches = str => {
+  const vals = str.split("'")
+  return 12 * vals[0] + vals[1]
+}
