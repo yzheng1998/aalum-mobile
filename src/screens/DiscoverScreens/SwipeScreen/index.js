2f5656d74356d191a56047175db3820a4aa4768e
Yuke Zheng <yukezheng@dhcp-10-250-125-215.harvard.edu>
2019-02-16 12:57:18 -0500

    Temp(Swiper) -- added preliminary swiper to screen

--- src/screens/DiscoverScreens/SwipeScreen/index.js
+++ src/screens/DiscoverScreens/SwipeScreen/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Screen, ButtonContainer } from './styles'
+import { AsyncStorage, View, Text } from 'react-native'
+import SwipeCards from 'react-native-swipe-cards'
 import UserCard from './components/UserCard'
 import SearchButton from './components/SearchButton'
 import ScreenHeader from '../../../components/ScreenHeader'
@@ -10,6 +12,162 @@ const SAMPLE_URL =
   'https://cdn-images-1.medium.com/max/2000/0*bdhf1cch4Mjib3UL.jpg'
 
 export default class SwipeScreen extends Component {
+  state = {
+    cards: [
+      {
+        name: 'Harley',
+        age: '25',
+        distance: '0.1',
+        school: 'Harvard University',
+        degree: 'B.A',
+        year: '2021',
+        picture: SAMPLE_URL
+      },
+      {
+        name: 'Harley',
+        age: '25',
+        distance: '0.1',
+        school: 'Harvard University',
+        degree: 'B.A',
+        year: '2021',
+        picture: SAMPLE_URL
+      },
+      {
+        name: 'Harley',
+        age: '25',
+        distance: '0.1',
+        school: 'Harvard University',
+        degree: 'B.A',
+        year: '2021',
+        picture: SAMPLE_URL
+      },
+      {
+        name: 'Harley',
+        age: '25',
+        distance: '0.1',
+        school: 'Harvard University',
+        degree: 'B.A',
+        year: '2021',
+        picture: SAMPLE_URL
+      },
+      {
+        name: 'Harley',
+        age: '25',
+        distance: '0.1',
+        school: 'Harvard University',
+        degree: 'B.A',
+        year: '2021',
+        picture: SAMPLE_URL
+      },
+      {
+        name: 'Harley',
+        age: '25',
+        distance: '0.1',
+        school: 'Harvard University',
+        degree: 'B.A',
+        year: '2021',
+        picture: SAMPLE_URL
+      },
+      {
+        name: 'Harley',
+        age: '25',
+        distance: '0.1',
+        school: 'Harvard University',
+        degree: 'B.A',
+        year: '2021',
+        picture: SAMPLE_URL
+      },
+      {
+        name: 'Harley',
+        age: '25',
+        distance: '0.1',
+        school: 'Harvard University',
+        degree: 'B.A',
+        year: '2021',
+        picture: SAMPLE_URL
+      },
+      {
+        name: 'Harley',
+        age: '25',
+        distance: '0.1',
+        school: 'Harvard University',
+        degree: 'B.A',
+        year: '2021',
+        picture: SAMPLE_URL
+      },
+      {
+        name: 'Harley',
+        age: '25',
+        distance: '0.1',
+        school: 'Harvard University',
+        degree: 'B.A',
+        year: '2021',
+        picture: SAMPLE_URL
+      },
+      {
+        name: 'Harley',
+        age: '25',
+        distance: '0.1',
+        school: 'Harvard University',
+        degree: 'B.A',
+        year: '2021',
+        picture: SAMPLE_URL
+      },
+      {
+        name: 'Harley',
+        age: '25',
+        distance: '0.1',
+        school: 'Harvard University',
+        degree: 'B.A',
+        year: '2021',
+        picture: SAMPLE_URL
+      },
+      {
+        name: 'Harley',
+        age: '25',
+        distance: '0.1',
+        school: 'Harvard University',
+        degree: 'B.A',
+        year: '2021',
+        picture: SAMPLE_URL
+      },
+      {
+        name: 'Harley',
+        age: '25',
+        distance: '0.1',
+        school: 'Harvard University',
+        degree: 'B.A',
+        year: '2021',
+        picture: SAMPLE_URL
+      },
+      {
+        name: 'Harley',
+        age: '25',
+        distance: '0.1',
+        school: 'Harvard University',
+        degree: 'B.A',
+        year: '2021',
+        picture: SAMPLE_URL
+      },
+      {
+        name: 'Harley',
+        age: '25',
+        distance: '0.1',
+        school: 'Harvard University',
+        degree: 'B.A',
+        year: '2021',
+        picture: SAMPLE_URL
+      }
+    ]
+  }
+  logout = async () => {
+    AsyncStorage.clear()
+    this.props.navigation.navigate('SignIn')
+  }
+
+  handleYup = () => {}
+  handleNope = () => {}
+
   render() {
     return (
       <Screen>
@@ -17,7 +175,19 @@ export default class SwipeScreen extends Component {
           <FilterButton navigation={this.props.navigation} />
         </ScreenHeader>
         <SearchButton navigation={this.props.navigation} />
-        <UserCard navigation={this.props.navigation} picture={SAMPLE_URL} />
+        <SwipeCards
+          cards={this.state.cards}
+          renderCard={cardData => (
+            <UserCard navigation={this.props.navigation} cardData={cardData} />
+          )}
+          renderNoMoreCards={() => (
+            <View>
+              <Text>No more cards</Text>
+            </View>
+          )}
+          handleYup={this.handleYup}
+          handleNope={this.handleNope}
+        />
         <ButtonContainer>
           <MatchButton name="close" />
           <MatchButton name="heart" />
