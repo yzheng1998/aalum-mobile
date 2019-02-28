a9e74d5d993b7141e561a83dfc5e909438ea479e
Mohamed Ally Iddi <mohamed_iddi@college.harvard.edu>
2019-02-27 21:28:58 -0500

    Styling(ApplyButton) - Made ApplyButton float over filter screen

--- src/screens/DiscoverScreens/FilterScreen/styles.js
+++ src/screens/DiscoverScreens/FilterScreen/styles.js
@@ -1,12 +1,4 @@
 import styled from 'styled-components'
-import { Dimensions } from 'react-native'
-
-const { height } = Dimensions.get('window')
-
-export const Screen = styled.ScrollView`
-  flex: 1;
-  padding-top: ${height * 0.07};
-`
 
 export const Container = styled.View`
   flex: 1;
