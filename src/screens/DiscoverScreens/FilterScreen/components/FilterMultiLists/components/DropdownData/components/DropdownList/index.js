faff4f0b6780d480a85ff42c79ca8be589a849e5
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2019-03-01 15:45:29 -0500

    Fix(DropdownList) -- make selection buttons appear on mount

--- src/screens/DiscoverScreens/FilterScreen/components/FilterMultiLists/components/DropdownData/components/DropdownList/index.js
+++ src/screens/DiscoverScreens/FilterScreen/components/FilterMultiLists/components/DropdownData/components/DropdownList/index.js
@@ -4,11 +4,20 @@ import { Container, SubContainer, searchInputStyle } from './styles'
 import theme from '../../../../../../../../../../theme'
 
 class DropdownList extends Component {
+  /* fixes error of selection buttons not appearing until
+  state updates by updating state as soon as component mounts */
+  state = {
+    refresh: false
+  }
+
+  componentDidMount = () => {
+    this.setState({ refresh: !this.state.refresh })
+  }
+
   render() {
     const { selectedItems, items, onChange, text } = this.props
     const { bodyText, title } = theme.fonts
     const { charcoal, gradientLeft, inactiveText } = theme.colors
-
     return (
       <Container>
         <SubContainer>
