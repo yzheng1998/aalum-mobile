23d03b14c6f5ec1deaebb506ec41b0544de8ae7c
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2019-03-02 09:28:56 -0500

    Func(UpdateLocation) -- enable location on app when it becomes active and when you sign in

--- App.js
+++ App.js
@@ -6,9 +6,42 @@ import store from './src/redux/store'
 import { client } from './src/apollo'
 import RootNavigator from './src/navigation/RootNavigator'
 import theme from './theme'
-import { YellowBox } from 'react-native'
+import { YellowBox, AppState, AsyncStorage, Alert } from 'react-native'
+import { setPosition } from './AppUtilities/geolocation'
 
 export default class App extends Component {
+  componentDidMount() {
+    AppState.addEventListener('change', this.handleAppStateChange)
+    this.isSignedIn()
+      .then(res => {
+        if (res) {
+          setPosition()
+        }
+      })
+      .catch(() => Alert.alert('Could not identify if user is signed in'))
+  }
+
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this.handleAppStateChange)
+  }
+
+  isSignedIn = async () => {
+    const token = await AsyncStorage.getItem('token')
+    return token !== null
+  }
+
+  handleAppStateChange = nextAppState => {
+    if (nextAppState === 'active') {
+      this.isSignedIn().then(res => {
+        if (res) {
+          setPosition()
+        }
+      })
+    }
+    /* if you try to set state when app is inactive, then the app resets
+    when it becomes inactive (e.g. when permissions dialog opens) */
+  }
+
   render() {
     YellowBox.ignoreWarnings([
       'Error: User cancelled image selection',
