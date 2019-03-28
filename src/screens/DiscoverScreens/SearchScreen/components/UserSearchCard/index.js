252ff8904d2921cc84f6929f63fb3f0d4b917128
Hossam Mabed <hossam.nasr99@gmail.com>
2019-03-27 21:52:43 -0400

    Styling(UserSearchCard) -- switched the UserSearchCard to the new general component

--- src/screens/DiscoverScreens/SearchScreen/components/UserSearchCard/index.js
+++ src/screens/DiscoverScreens/SearchScreen/components/UserSearchCard/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { TouchableOpacity, Alert } from 'react-native'
-import { Container, ContentContainer, HeartContainer } from './styles'
+import { HeartContainer } from './styles'
 import Heart from '../../../../../components/Heart'
-import UserSearchCardPicture from './components/UserSearchCardPicture'
-import UserSearchCardDetails from './components/UserSearchCardDetails'
+import UserCard from '../../../../../components/UserCard'
 import { SEND_MATCH_RESPONSE } from './mutations'
 import { Mutation } from 'react-apollo'
 
@@ -20,43 +19,37 @@ const UserSearchCard = ({
   navigation,
   refetch
 }) => (
-  <Container
+  <UserCard
     onPress={() => navigation.navigate('User', { id, searchRefetch: refetch })}
+    profilePicture={profilePicture}
+    name={name}
+    age={age}
+    locationName={locationName}
+    school={school}
+    degree={degree}
+    year={year}
   >
-    <ContentContainer>
-      <UserSearchCardPicture profilePicture={profilePicture} />
-      <UserSearchCardDetails
-        name={name}
-        age={age}
-        locationName={locationName}
-        school={school}
-        degree={degree}
-        year={year}
-      />
-      <HeartContainer>
-        <Mutation
-          mutation={SEND_MATCH_RESPONSE}
-          onCompleted={() => refetch()}
-          onError={error => {
-            if (error) {
-              Alert.alert('Encountered server error')
-            }
-          }}
-        >
-          {sendMatchResponse => {
-            const variables = { recipient: id, swipedRight: !swipedRight }
-            return (
-              <TouchableOpacity
-                onPress={() => sendMatchResponse({ variables })}
-              >
-                <Heart active={swipedRight} width="28px" height="24.25px" />
-              </TouchableOpacity>
-            )
-          }}
-        </Mutation>
-      </HeartContainer>
-    </ContentContainer>
-  </Container>
+    <HeartContainer>
+      <Mutation
+        mutation={SEND_MATCH_RESPONSE}
+        onCompleted={() => refetch()}
+        onError={error => {
+          if (error) {
+            Alert.alert('Encountered server error')
+          }
+        }}
+      >
+        {sendMatchResponse => {
+          const variables = { recipient: id, swipedRight: !swipedRight }
+          return (
+            <TouchableOpacity onPress={() => sendMatchResponse({ variables })}>
+              <Heart active={swipedRight} width="28px" height="24.25px" />
+            </TouchableOpacity>
+          )
+        }}
+      </Mutation>
+    </HeartContainer>
+  </UserCard>
 )
 
 export default UserSearchCard
