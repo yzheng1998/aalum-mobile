0482568ab36e36a8a769bb83732f6a8eccbbcfba
Kyle Sutton <ksutton@dhcp-10-250-226-77.harvard.edu>
2019-04-03 15:56:52 -0400

    fixed config

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  graphqlUrl:
-    process.env.REACT_APP_API_URL || 'https://dcf46178.ngrok.io/graphql',
+  graphqlUrl: process.env.REACT_APP_API_URL || 'http://localhost:5000/graphql',
   graphqlWsUrl:
     process.env.REACT_APP_WS_API_URL || 'ws://localhost:5000/subscriptions',
   gcmSenderId: process.env.GCM_SENDER_ID || '453609108319'
