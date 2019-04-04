558dd0197f61025cf22c7663826f641132ff771e
Hossam Mabed <hossam.nasr99@gmail.com>
2019-04-03 21:45:22 -0400

    Sustain(Refactoring) -- using the screen general component MessageScreen

--- src/screens/MessageScreens/MessagesScreen/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-import { Dimensions } from 'react-native'
-
-const { height } = Dimensions.get('window')
-
-export const Screen = styled.View`
-  flex: 1;
-  padding-top: ${height * 0.065};
-`
