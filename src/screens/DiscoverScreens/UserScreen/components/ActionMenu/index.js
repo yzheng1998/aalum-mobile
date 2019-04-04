78dc0aea8c4e61a01aff657f8a178182246e339a
Hossam Mabed <hossam.nasr99@gmail.com>
2019-04-03 20:23:06 -0400

    Sustain(SFC) -- UserScreen components

--- src/screens/DiscoverScreens/UserScreen/components/ActionMenu/index.js
+++ src/screens/DiscoverScreens/UserScreen/components/ActionMenu/index.js
@@ -1,30 +1,52 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ActionSheet from 'react-native-actionsheet'
 import { Alert } from 'react-native'
 import { Mutation } from 'react-apollo'
 import { REPORT_USER, UNMATCH_USER } from '../../mutations'
 
-export default class ActionMenu extends Component {
-  render() {
-    const {
-      id,
-      setActionsheet,
-      searchRefetch,
-      discoveryRefetch,
-      userRefetch
-    } = this.props
-    return (
+const ActionMenu = ({
+  id,
+  setActionsheet,
+  searchRefetch,
+  discoveryRefetch,
+  userRefetch,
+  navigation
+}) => (
+  <Mutation
+    mutation={REPORT_USER}
+    onCompleted={reportData => {
+      if (reportData.reportUser.success) {
+        Alert.alert('You reported this user.', '', [
+          {
+            text: 'OK',
+            onPress: () => {
+              if (searchRefetch) searchRefetch()
+              if (discoveryRefetch) discoveryRefetch()
+              navigation.goBack()
+            }
+          }
+        ])
+      }
+    }}
+    onError={error => {
+      if (error) {
+        Alert.alert('Encountered server error')
+      }
+    }}
+  >
+    {reportUser => (
       <Mutation
-        mutation={REPORT_USER}
-        onCompleted={reportData => {
-          if (reportData.reportUser.success) {
-            Alert.alert('You reported this user.', '', [
+        mutation={UNMATCH_USER}
+        onCompleted={unmatchData => {
+          userRefetch()
+          if (unmatchData.sendMatchResponse.success) {
+            Alert.alert('You unmatched with this user', '', [
               {
                 text: 'OK',
                 onPress: () => {
                   if (searchRefetch) searchRefetch()
                   if (discoveryRefetch) discoveryRefetch()
-                  this.props.navigation.goBack()
+                  navigation.goBack()
                 }
               }
             ])
@@ -36,55 +58,31 @@ export default class ActionMenu extends Component {
           }
         }}
       >
-        {reportUser => (
-          <Mutation
-            mutation={UNMATCH_USER}
-            onCompleted={unmatchData => {
-              userRefetch()
-              if (unmatchData.sendMatchResponse.success) {
-                Alert.alert('You unmatched with this user', '', [
-                  {
-                    text: 'OK',
-                    onPress: () => {
-                      if (searchRefetch) searchRefetch()
-                      if (discoveryRefetch) discoveryRefetch()
-                      this.props.navigation.goBack()
-                    }
-                  }
-                ])
+        {unmatchUser => (
+          <ActionSheet
+            ref={setActionsheet}
+            options={['Report', 'Unmatch', 'Cancel']}
+            cancelButtonIndex={2}
+            destructiveButtonIndex={1}
+            onPress={index => {
+              if (index === 0) {
+                const variables = {
+                  reportedUserId: id
+                }
+                reportUser({ variables })
+              } else if (index === 1) {
+                const variables = {
+                  recipient: id,
+                  swipedRight: false
+                }
+                unmatchUser({ variables })
               }
             }}
-            onError={error => {
-              if (error) {
-                Alert.alert('Encountered server error')
-              }
-            }}
-          >
-            {unmatchUser => (
-              <ActionSheet
-                ref={setActionsheet}
-                options={['Report', 'Unmatch', 'Cancel']}
-                cancelButtonIndex={2}
-                destructiveButtonIndex={1}
-                onPress={index => {
-                  if (index === 0) {
-                    const variables = {
-                      reportedUserId: id
-                    }
-                    reportUser({ variables })
-                  } else if (index === 1) {
-                    const variables = {
-                      recipient: id,
-                      swipedRight: false
-                    }
-                    unmatchUser({ variables })
-                  }
-                }}
-              />
-            )}
-          </Mutation>
+          />
         )}
       </Mutation>
-    )
-  }
-}
+    )}
+  </Mutation>
+)
+
+export default ActionMenu
--- src/screens/DiscoverScreens/UserScreen/components/MatchButtons/index.js
+++ src/screens/DiscoverScreens/UserScreen/components/MatchButtons/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Alert } from 'react-native'
 import LinearGradient from 'react-native-linear-gradient'
 import MatchButton from '../../../../../components/MatchButton'
@@ -12,74 +12,72 @@ import theme from '../../../../../../theme'
 import { Mutation } from 'react-apollo'
 import { SEND_MATCH_RESPONSE } from './mutations'
 
-export default class MatchButtons extends Component {
-  render() {
-    const { buttonShadow1, buttonShadow2, offWhite } = theme.colors
-    const {
-      recipient,
-      refetch,
-      navigation,
-      searchRefetch,
-      discoveryRefetch
-    } = this.props
-    return (
-      <Container>
-        <LinearGradient
-          start={{ x: 0, y: 0 }}
-          end={{ x: 0, y: 1 }}
-          colors={[buttonShadow1, buttonShadow2, offWhite]}
-          locations={[0, 0.3415, 1]}
-          style={gradientStyle}
-        >
-          <InnerContainer>
-            <ButtonContainer>
-              <Mutation
-                mutation={SEND_MATCH_RESPONSE}
-                onCompleted={() => {
-                  refetch()
-                  if (searchRefetch) searchRefetch()
-                  if (discoveryRefetch) discoveryRefetch()
-                  navigation.goBack()
-                }}
-                onError={error => {
-                  if (error) {
-                    Alert.alert('Encountered server error')
-                  }
-                }}
-              >
-                {sendMatchResponse => {
-                  const variables = { recipient, swipedRight: false }
-                  return (
-                    <MatchButton
-                      name="close"
-                      onPress={() => sendMatchResponse({ variables })}
-                    />
-                  )
-                }}
-              </Mutation>
-
-              <Mutation
-                mutation={SEND_MATCH_RESPONSE}
-                onCompleted={() => {
-                  refetch()
-                  if (searchRefetch) searchRefetch()
-                  if (discoveryRefetch) discoveryRefetch()
-                }}
-              >
-                {sendMatchResponse => {
-                  const variables = { recipient, swipedRight: true }
-                  return (
-                    <MatchButton
-                      name="heart"
-                      onPress={() => sendMatchResponse({ variables })}
-                    />
-                  )
-                }}
-              </Mutation>
-            </ButtonContainer>
-          </InnerContainer>
-        </LinearGradient>
-      </Container>
-    )
-  }
+const MatchButtons = ({
+  recipient,
+  refetch,
+  navigation,
+  searchRefetch,
+  discoveryRefetch
+}) => {
+  const { buttonShadow1, buttonShadow2, offWhite } = theme.colors
+  return (
+    <Container>
+      <LinearGradient
+        start={{ x: 0, y: 0 }}
+        end={{ x: 0, y: 1 }}
+        colors={[buttonShadow1, buttonShadow2, offWhite]}
+        locations={[0, 0.3415, 1]}
+        style={gradientStyle}
+      >
+        <InnerContainer>
+          <ButtonContainer>
+            <Mutation
+              mutation={SEND_MATCH_RESPONSE}
+              onCompleted={() => {
+                refetch()
+                if (searchRefetch) searchRefetch()
+                if (discoveryRefetch) discoveryRefetch()
+                navigation.goBack()
+              }}
+              onError={error => {
+                if (error) {
+                  Alert.alert('Encountered server error')
+                }
+              }}
+            >
+              {sendMatchResponse => {
+                const variables = { recipient, swipedRight: false }
+                return (
+                  <MatchButton
+                    name="close"
+                    onPress={() => sendMatchResponse({ variables })}
+                  />
+                )
+              }}
+            </Mutation>
+            <Mutation
+              mutation={SEND_MATCH_RESPONSE}
+              onCompleted={() => {
+                refetch()
+                if (searchRefetch) searchRefetch()
+                if (discoveryRefetch) discoveryRefetch()
+              }}
+            >
+              {sendMatchResponse => {
+                const variables = { recipient, swipedRight: true }
+                return (
+                  <MatchButton
+                    name="heart"
+                    onPress={() => sendMatchResponse({ variables })}
+                  />
+                )
+              }}
+            </Mutation>
+          </ButtonContainer>
+        </InnerContainer>
+      </LinearGradient>
+    </Container>
+  )
 }
+
+export default MatchButtons
