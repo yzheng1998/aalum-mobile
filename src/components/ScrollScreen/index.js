efa90bf0db3c4bc0faac470a28a32facc9de48b4
Hossam Mabed <hossam.nasr99@gmail.com>
2019-04-03 21:24:19 -0400

    Sustain(Refactoring) -- new general components Screen and ScrollScreen

---
+++ src/components/ScrollScreen/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import { Dimensions } from 'react-native'
+
+const { height } = Dimensions.get('window')
+
+const Screen = styled.ScrollView`
+  flex: 1;
+  padding-top: ${height * 0.065};
+`
+
+export default Screen
