9442014fac6bd39261ba4583908b91822de27d0d
Hossam Mabed <hossam.nasr99@gmail.com>
2019-04-03 21:42:26 -0400

    Sustain(Refactoring) -- using the screen general component for SearchScreen

--- src/screens/DiscoverScreens/SearchScreen/styles.js
+++ src/screens/DiscoverScreens/SearchScreen/styles.js
@@ -1,13 +1,5 @@
 import styled from 'styled-components'
 import { themeGet } from 'styled-system'
-import { Dimensions } from 'react-native'
-
-const { height } = Dimensions.get('window')
-
-export const Screen = styled.View`
-  flex: 1;
-  padding-top: ${height * 0.07};
-`
 
 export const SearchHeader = styled.View`
   border-bottom-width: 1px;
