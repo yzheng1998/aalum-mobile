176396284303fc8c7122d4b3358a4e69702cf1b5
Yuke Zheng <yukezheng@dhcp-10-250-134-154.harvard.edu>
2019-04-08 17:46:53 -0400

    Fix(registraionScreenComponents) -- fix bug with registration screen imports

--- src/screens/RegistrationScreens/components/RegistrationScreen/styles.js
+++ src/screens/RegistrationScreens/components/RegistrationScreen/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import theme from '../../../theme'
+import theme from '../../../../../theme'
 
 export const SafeAreaView = styled.SafeAreaView`
   flex: 1;
--- src/screens/RegistrationScreens/components/Subtitle/index.js
+++ src/screens/RegistrationScreens/components/Subtitle/index.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import theme from '../../../theme'
+import theme from '../../../../../theme'
 
 export const Subtitle = styled.Text`
   align-self: center;
